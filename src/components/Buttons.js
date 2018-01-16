6a126109edafc7c5e044d0b3abddf400845d96b1
bui quang tuan <tuanbq@vietinbanksc.vn>
2018-01-16 17:23:53 +0700

    create upload module

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -5,21 +5,56 @@ import './Buttons.css';
 var UploadLabel = MaterialUI.InputLabel;
 var BtnUpload = MaterialUI.Button;
 
-const BrowseBtnRender = () => {
+const BrowseBtnRender = (props) => {
+
+	var handleImageFile = () => {
+		var fileReader = new FileReader();
+		props.imgPath(this.uploadBtn.files[0].path);
+	}
+
 	return (
-		<UploadLabel>
-			<BtnUpload raised color="primary" onClick={() => this.uploadBtn.click()}>Upload</BtnUpload>
-			<input type="file" ref={(r) => {this.uploadBtn = r;}} style={{display: 'none'}}/>
+		<UploadLabel title={props.title}>
+			<BtnUpload raised color="primary" onClick={() => this.uploadBtn.click()}>Browse</BtnUpload>
+			<input type="file" ref={(r) => {this.uploadBtn = r;}} 
+				style={{display: 'none'}}
+				accept="image/x-png,image/gif,image/jpeg"
+				onChange={() => {
+					handleImageFile();
+				}}/>
 		</UploadLabel>
 	);
 }
 
+const ImageViewer = () => {
+
+}
+
 export class BrowseFilePannel extends React.Component {
+	constructor(props) {
+		super(props);
+		var imgFile;
+		this.state = {
+			imgPath: ""
+		}
+	}
+
+	componentWillMount() {
+		console.log(this.state.imgPath);
+	}
 
 	render() {
 		return (
 			<div>
-				<BrowseBtnRender/>
+				<form method="POST">
+					<BrowseBtnRender imgPath={(p) => {this.setState({imgPath : p});}}/>
+					<br/>
+					<br/>
+					<BtnUpload raised color="primary" onClick={(e) => {
+						console.log(this.imgFile.props.filePath);
+					}}>Submit</BtnUpload>
+				</form>
+				<br/>
+				{this.state.imgPath === "" ? null : <img src={require('./headphone3.jpg')} width="300" height="377" alt="Headphone"/>}
 			</div>
 		);
 	}
